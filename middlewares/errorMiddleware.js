da35599f5580531c3cf7320c5490e04170cc2a0f
hd <classylifestyling@gmail.com>
2024-01-08 16:56:54 +0530

    errorMIddleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,7 +1,7 @@
 export const errorMiddleware = (err, req, res, next) => {
 
     err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || "500";
+    err.statusCode = err.statusCode || 500;
 
     res.status(err.statusCode).json({
         success: false,
