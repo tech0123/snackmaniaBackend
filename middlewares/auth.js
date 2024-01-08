98cbb6b23de96502a59fb17934b58dc75a49e6e4
hd <classylifestyling@gmail.com>
2024-01-08 16:48:52 +0530

    ordermodelFINAL

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,15 +7,12 @@ export const isAuthenticated = (req, res, next) => {
     if (!token) {
         return next(new ErrorHandler("Not Logged In", 401));
     }
-    console.log(req.user._id)
-
 
     next();
 }
 
 export const authorizeAdmin = (req, res, next) => {
 
-    console.log(req.user._id)
     if (req.user.role !== "admin") {
         return next(new ErrorHandler("Only Admin Allowed", 405));
     }
