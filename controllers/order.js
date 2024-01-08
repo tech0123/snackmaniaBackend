8cee292c7a0404da74aaf9e98d4430fc39f7caed
hd <classylifestyling@gmail.com>
2024-01-08 16:33:03 +0530

    ordermodel1

--- controllers/order.js
+++ controllers/order.js
@@ -9,7 +9,10 @@ export const placeOrder = asyncError(
     async (req, res, next) => {
 
         const { shippingInfo, orderItems, paymentMethod, itemsPrice, taxPrice, shippingCharges, totalAmount } = req.body;
-        const user = req.user._id;
+        const user = req.user && req.user._id;
+        console.log("placeOrder - userid:", user);
+        console.log("placeOrder - user:", req.user);
+
 
         const orderOptions = {
             shippingInfo, orderItems, paymentMethod, itemsPrice, taxPrice, shippingCharges, totalAmount, user
